65c50bb8b384db40ab8e8e1c9562c82483615c27
zittawardani <zittawardani@gmail.com>
2025-06-01 22:56:06 +0700

    checkbox signup

--- src/pages/user/signup/index.tsx
+++ src/pages/user/signup/index.tsx
@@ -230,11 +230,7 @@ export default function Signup() {
                 />
               </div>
               <div className="flex items-center gap-2  w-fit">
-                <Checkbox
-                  id="terms"
-                  checked={isTermsAgreed}
-                  onChange={handleCheckboxChange}
-                />
+                <Checkbox id="terms" onChange={handleCheckboxChange} />
                 <Label htmlFor="terms" className="text-sm">
                   I agree to the terms and conditions
                 </Label>
